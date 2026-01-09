b2e559e797e686795af4ba6021e9c6d0e5a04eed
Sam Lake <s.lake@gripple.com>
2026-01-09 14:16:20 +0000

    BI Modified

---
+++ docker_mounting/app.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from datetime import datetime
+
+out = Path("/data/hello.txt")
+
+with out.open("a") as f:
+    f.write(f"Hello from a bind mount. The current date and time is {datetime.now()}\n")
+
+    
+
